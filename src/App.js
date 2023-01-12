ad1c6b96d48b4dd67b3bf5376ebfd6766441dada
Igwe Acha Akwu <successoracha1995@gmail.com>
2023-01-12 21:08:48 +0100

    reset home routes

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/dashboard" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
