532de58c31752af4ce1ffe1f4603b9a6676644ab
Igwe Acha Akwu <successoracha1995@gmail.com>
2023-01-13 05:44:42 +0100

    added bootstrap version 4.1.1

---
+++ src/components/common/AppIcon.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./../../assets/logo-7.png";
+
+export const AppIcon = () => {
+  return (
+    <div>
+      <img src={logo} alt="App Icon" />
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.js";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import $ from "jquery";
+// import "bootstrap/dist/js/bootstrap.min.js";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
