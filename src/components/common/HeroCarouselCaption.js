7966a6a17d2101302ae00519eb3350e53d2e4667
Igwe Acha Akwu <successoracha1995@gmail.com>
2023-01-14 13:45:27 +0100

    finished hero section

--- src/components/common/HeroCarouselCaption.js
+++ src/components/common/HeroCarouselCaption.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./herocarouselcaption.css";
+import { Link } from "react-router-dom";
 
 export const HeroCarouselCaption = () => {
   return (
     <div className="carousel-caption">
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <div>We work for your success</div>
+      <h1>We build your best business</h1>
+      <p>
+        Our banking services are designed to give our customers rest of mind and
+        100% satisfaction.
+      </p>
+      <div className="d-flex">
+        <Link className="btn btn-primary btn-outline-primary contact" to="/">
+          Contact Us
+        </Link>
+        <Link className="btn btn-primary mx-4 service" to="/">
+          Services
+        </Link>
+      </div>
     </div>
   );
 };
