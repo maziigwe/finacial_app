22b36cd1bd52f0eb91dc66b02b36766931265ff4
Igwe Acha Akwu <successoracha1995@gmail.com>
2023-01-14 12:50:13 +0100

    added fixed layout issue

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,10 +1,60 @@
 import React from "react";
+import { HeroCarouselCaption } from "./common/HeroCarouselCaption";
 import "./herosection.css";
 
 export const HeroSection = () => {
   return (
     <div>
-      <h1>Hero Section</h1>
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide"
+        data-ride="carousel"
+      >
+        <ol className="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active carousel-image-1">
+            <HeroCarouselCaption />
+          </div>
+          <div className="carousel-item carousel-image-2">
+            <HeroCarouselCaption />
+          </div>
+          <div className="carousel-item carousel-image-1">
+            <HeroCarouselCaption />
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
     </div>
   );
 };
---
+++ src/components/common/HeroCarouselCaption.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./herocarouselcaption.css";
+
+export const HeroCarouselCaption = () => {
+  return (
+    <div className="carousel-caption">
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </div>
+  );
+};
