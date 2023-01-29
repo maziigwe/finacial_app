948346786084e54c4e602701a989db430a029b17
Igwe Acha Akwu <successoracha1995@gmail.com>
2023-01-29 12:59:34 +0100

    set font-size

--- src/components/common/StatisticsItem.js
+++ src/components/common/StatisticsItem.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./statistics-item.css";
 
-export const StatisticsItem = () => {
+export const StatisticsItem = ({
+  title = "Accounts Opened",
+  total = "120k",
+}) => {
   return (
     <div className="statistics-item">
-      <div>Icon</div> <div>Total</div> <div>Title</div>
+      <div>Icon</div>{" "}
+      <div>
+        <h3 className="total">{total}</h3>
+      </div>
+      <div>
+        <h4 className="title">{title}</h4>
+      </div>
     </div>
   );
 };
