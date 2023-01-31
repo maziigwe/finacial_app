997e2147a6ac91a2d792c7455d1e3153b0d5d3a4
Igwe Acha Akwu <successoracha1995@gmail.com>
2023-01-31 08:18:23 +0100

    finished news 1

--- src/components/NewsSection.js
+++ src/components/NewsSection.js
@@ -6,7 +6,7 @@ import { GridLayout1x3 } from "./../component/GridLayout1x3";
 
 export const NewsSection = () => {
   return (
-    <div className="container news-section">
+    <div className="container news-section mb-4">
       <div>
         <div className="mb-4 px-4 section-left-border">
           <div className="section-title ">SOME OF OUR NEWS </div>
@@ -15,8 +15,16 @@ export const NewsSection = () => {
         <div className="mt-4 pt-4">
           <GridLayout1x3>
             <NewsItem />
-            <NewsItem />
-            <NewsItem />
+            <NewsItem
+              date="January 05, 2023"
+              title="What we are capable of usually gets discovered."
+              image="https://diamond-continental.com/images/resource/news-5.jpg"
+            />
+            <NewsItem
+              date="January 15, 2023"
+              title="Food industry leaders often change their promoters."
+              image="https://diamond-continental.com/images/resource/news-6.jpg"
+            />
           </GridLayout1x3>
         </div>
       </div>
